601e1067c478131794b3daa456fa87c3ef89a7b4
Sina Ataei <sinaataei@outlook.com>
2024-03-04 22:15:22 +0000

    IAccountRepository added

---
+++ PersonalFinance.Repository/IAccountRepository.cs
@@ -0,0 +1,14 @@
+﻿using System.Data.Common;
+using PersonalFinance.Shared;
+
+namespace PersonalFinance.Repository;
+
+public interface IAccountRepository
+{
+    public Task<Account> CreateAccountAsync(Account account);
+    public Task<Account> GetAccountAsync(int accountId);
+    public Task<Account> UpdateAccountAsync(Account account);
+    public Task<Account> DeleteAccountAsync(int accountId);
+    public Task<IEnumerable<Account>> GetAccountsAsync();
+    public Task<IEnumerable<Account>> SearchAccountAsync(int? accountId, string? name);
+}
