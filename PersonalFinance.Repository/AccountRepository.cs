dd1296cf504e0013f6d7863579aca8875120bbb9
Sina Ataei <sinaataei@outlook.com>
2024-03-04 22:15:59 +0000

    CRUD operations for Account added.

--- PersonalFinance.Repository/AccountRepository.cs
+++ PersonalFinance.Repository/AccountRepository.cs
@@ -1,6 +1,164 @@
-﻿namespace PersonalFinance.Repository;
+﻿using System.Diagnostics;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Logging;
+using PersonalFinance.Shared;
 
-public class AccountRepository
+namespace PersonalFinance.Repository;
+
+public class AccountRepository : IAccountRepository
 {
-    
+    private readonly PFDbContext dbContext;
+    private readonly ILogger logger;
+
+    public AccountRepository(PFDbContext dbContext, ILogger logger)
+    {
+        this.dbContext = dbContext;
+        this.logger = logger;
+    }
+
+    public async Task<Account> CreateAccountAsync(Account account)
+    {
+        try
+        {
+            if (account != null)
+            {
+                var res = await dbContext.Accounts.AddAsync(new Account { Name = account.Name });
+                if (res != null)
+                {
+                    await dbContext.SaveChangesAsync();
+                    return res.Entity;
+                }
+            }
+            throw new Exception("Error creating new account.", new NullReferenceException());
+        }
+        catch (Exception e)
+        {
+            await Task.Run(() =>
+            {
+                logger.LogError(e.Message);
+            });
+            return null;
+        }
+    }
+
+    public async Task<Account> DeleteAccountAsync(int accountId)
+    {
+        try
+        {
+            var account = await dbContext.Accounts.FirstOrDefaultAsync(A => A.Id == accountId);
+
+            if (account != null)
+            {
+                var res = dbContext.Accounts.Remove(account);
+                await dbContext.SaveChangesAsync();
+
+                return res.Entity;
+            }
+            throw new Exception($"Error deleting account with {{Id = {accountId}}}.", new NullReferenceException());
+        }
+        catch (Exception e)
+        {
+            await Task.Run(() =>
+            {
+                logger.LogError(e.Message);
+            });
+            return null;
+        }
+    }
+
+    public async Task<Account> GetAccountAsync(int accountId)
+    {
+        try
+        {
+            var account = await dbContext.Accounts.FirstOrDefaultAsync(A => A.Id == accountId);
+
+            if (account != null)
+            {
+                return account;
+            }
+            throw new Exception($"Error retrieving account with {{Id = {accountId}}}.", new NullReferenceException());
+        }
+        catch (Exception e)
+        {
+            await Task.Run(() =>
+            {
+                logger.LogError(e.Message);
+            });
+            return null;
+        }
+    }
+
+    public async Task<IEnumerable<Account>> GetAccountsAsync()
+    {
+        try
+        {
+            return await dbContext.Accounts.ToListAsync();
+        }
+        catch (Exception e)
+        {
+            await Task.Run(() =>
+            {
+                logger.LogError(e.Message);
+            });
+            return null;
+        }
+    }
+
+    public async Task<IEnumerable<Account>> SearchAccountAsync(int? accountId, string? name)
+    {
+        try
+        {
+
+            var query = dbContext.Accounts.AsQueryable();
+
+            await Task.Run(() =>
+            {
+                if (accountId != null)
+                {
+                    query = query.Where(a => a.Id == accountId);
+                }
+                if (name != null)
+                {
+                    query = query.Where(a => a.Name.Contains(name));
+                }
+            });
+
+            return await query.ToListAsync();
+        }
+        catch (Exception e)
+        {
+            await Task.Run(() =>
+            {
+                logger.LogError(e.Message);
+            });
+            return null;
+        }
+    }
+
+    public async Task<Account> UpdateAccountAsync(Account account)
+    {
+        try
+        {
+            var accountToEdit = await dbContext.Accounts.FindAsync(account.Id);
+
+            if (accountToEdit != null)
+            {
+                accountToEdit.Name = account.Name;
+
+                var res = dbContext.Accounts.Update(accountToEdit);
+                await dbContext.SaveChangesAsync();
+
+                return res.Entity;
+            }
+            return null;
+        }
+        catch (Exception e)
+        {
+            await Task.Run(() =>
+            {
+                logger.LogError(e.Message);
+            });
+            return null;
+        }
+    }
 }
