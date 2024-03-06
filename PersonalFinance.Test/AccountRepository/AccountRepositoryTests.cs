38fccf8fa783b4a9bbbebeff2911b84924ba67cf
Sina Ataei <sinaataei@outlook.com>
2024-03-06 21:29:00 +0000

    New test cases added to `AccountRepositoryTests`

--- PersonalFinance.Test/AccountRepository/AccountRepositoryTests.cs
+++ PersonalFinance.Test/AccountRepository/AccountRepositoryTests.cs
@@ -1,29 +1,56 @@
-﻿using FluentAssertions;
+﻿using FakeItEasy;
+using FluentAssertions;
 using Microsoft.EntityFrameworkCore.Migrations.Operations;
 using Microsoft.Extensions.Logging;
 using PersonalFinance.Repository;
+using PersonalFinance.Shared;
 
 namespace PersonalFinance.Test;
 
 public class AccountRepositoryTests
 {
+    private readonly ILogger logger;
+    private readonly PFDbContext dbContext;
+    private readonly IAccountRepository accRepo;
+    public AccountRepositoryTests()
+    {
+        //Dependencies
+        // logger = A.Fake<ILogger>();
+        // dbContext = A.Fake<PFDbContext>();
+        accRepo = A.Fake<IAccountRepository>(x =>
+        {
+            //x.WithArgumentsForConstructor(() => new AccountRepository(dbContext, logger));
+        });
+    }
+
     [Fact]
     public async void AccountRepository_GetAccounts_ReturnList()
     {
         //Arrange
-        using ILoggerFactory factory = LoggerFactory.Create(builder =>
-        {
-            builder.AddConsole();
-        });
-        var logger = factory.CreateLogger(nameof(AccountRepository_GetAccounts_ReturnList));
-        var dbContext = new PFDbContext();
-        var accRepo = new AccountRepository(dbContext, logger);
+        A.CallTo(() => accRepo.GetAccountsAsync())
+            .Returns(Task.FromResult(A.CollectionOfDummy<Account>(10).AsEnumerable()));
 
         //Act
         var res = await accRepo.GetAccountsAsync();
 
         //Assert
+        res.Should().BeOfType<List<Account>>();
+        res.Should().HaveCount(10);
+    }
+
+    [Theory]
+    [InlineData(0)]
+    public async void AccountRepository_GetAccount_ReturnAccount(int accountId)
+    {
+        //Arrange
+        //A.CallTo(() => accRepo.GetAccountAsync(accountId))
+            //.Returns(Task.FromResult(A.Dummy<Account>()));
+
+        //Act
+        var res = await accRepo.GetAccountAsync(accountId);
+
+        //Assert
+        res.Should().BeOfType<Account?>();
         res.Should().NotBeNull();
-        res.Should().HaveCountGreaterThan(0);
     }
 }
