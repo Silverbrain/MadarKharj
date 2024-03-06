a2bd04a132cfdd874a2cc5fd1d32b5f20568aadc
Sina Ataei <sinaataei@outlook.com>
2024-03-06 16:20:17 +0000

    implementing tests for the Repository functionality

--- PersonalFinance.Test/Program.cs
+++ PersonalFinance.Test/Program.cs
@@ -2,73 +2,28 @@
 using System.Net.Http.Headers;
 using Microsoft.EntityFrameworkCore.Query;
 using Microsoft.EntityFrameworkCore.Query.SqlExpressions;
+using Microsoft.Extensions.Logging;
 using Microsoft.IdentityModel.Tokens;
 using PersonalFinance.Repository;
 using PersonalFinance.Shared;
+using PersonalFinance.Test;
 
-PFDbContext pFDb = new PFDbContext();
-var dbAccess= new DataAccessTest(pFDb);
+var dbContext = new PFDbContext();
+ILogger logger;
 
-// Console.Write("Input the account name >");
-// var newAccName = Console.ReadLine();
+using ILoggerFactory factory = LoggerFactory.Create(builder => {
+            builder.AddConsole();
+        });
+        logger = factory.CreateLogger("Test Console");
 
-// if(!newAccName.IsNullOrEmpty())
-// {
-//     var res = dbAccess.CreateAccount(new Account{Name = newAccName});
-//     Console.WriteLine(res);
-// }
+var accRepo = new AccountRepository(dbContext, logger);
+var tranRepo = new TransactionRepository(dbContext, logger);
+var entRepo = new EntryRepository(dbContext, logger);
 
-// foreach(var acc in dbAccess.GetAccounts())
-// {
-//     Console.WriteLine($"Id: {acc.Id}, Name: {acc.Name}");
-// }
+var repo = new RepositoryTest(accRepo, tranRepo, entRepo);
 
-// Console.Write("Input the account Id to be deleted > ");
-// int accId = int.Parse(Console.ReadLine());
-
-// var res = dbAccess.DeleteAccount(accId);
-// Console.WriteLine(res);
-
-// foreach(var acc in dbAccess.GetAccounts())
-// {
-//     Console.WriteLine($"Id: {acc.Id}, Name: {acc.Name}");
-// }
-
-// Transaction tran = new Transaction{Id = Guid.NewGuid(), Date = DateTime.Now, Description = "New transaction for test"};
-// var res = dbAccess.CreateTransaction(tran);
-// Console.WriteLine(res);
-//"442f76e0-a619-45c8-960c-4ff3f4b9f136"
-
-// var tranId = new Guid("442f76e0-a619-45c8-960c-4ff3f4b9f136");
-
-// Console.Write("Input the amount < 1 (Credit) for the entry >\n");
-// var amount = float.Parse(Console.ReadLine());
-
-// Console.Write("Input the account id for this entry > ");
-// var accId = int.Parse(Console.ReadLine());
-
-// Entry credEntry = new Entry{Id = Guid.NewGuid(), AccountId = accId, Amount = amount, TransactionId = tranId};
-
-// Console.WriteLine(dbAccess.CreateEntry(credEntry));
-
-// Console.Write("Input the amount > 1 (Debit) for the entry >\n");
-// amount = float.Parse(Console.ReadLine());
-
-// Console.Write("Input the account id for this entry > ");
-// accId = int.Parse(Console.ReadLine());
-
-// Entry debEntry = new Entry{Id = Guid.NewGuid(), AccountId = accId, Amount = amount, TransactionId = tranId};
-
-// Console.WriteLine(dbAccess.CreateEntry(debEntry));
-
-foreach(var tran in dbAccess.GetTransactions())
+var accs = await repo.GetAccounts();
+foreach(Account account in accs)
 {
-    var fromAcc = tran.Entries.First(e => e.Amount < 0).Account;
-    var toAcc = tran.Entries.First(e => e.Amount > 0).Account;
-    var debEntry = tran.Entries.First(e => e.Amount > 0);
-    Console.WriteLine($@"Transaction Id: {tran.Id}
-Date: {tran.Date}
-From: {fromAcc.Name}
-To: {toAcc.Name}
-Amount: {debEntry.Amount}");
+    Console.WriteLine(account);
 }
---
+++ PersonalFinance.Test/RepositoryTest.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations.Operations;
+using PersonalFinance.Repository;
+using PersonalFinance.Shared;
+
+namespace PersonalFinance.Test;
+
+public class RepositoryTest
+{
+    private readonly IAccountRepository accountRepository;
+    private readonly ITransactionRepository transactionRepository;
+    private readonly IEntryRepository entryRepository;
+
+    public RepositoryTest(
+        IAccountRepository accountRepository,
+        ITransactionRepository transactionRepository,
+        IEntryRepository entryRepository)
+    {
+        this.accountRepository = accountRepository;
+        this.transactionRepository = transactionRepository;
+        this.entryRepository = entryRepository;
+    }
+
+    public async void AddAccount(string accName)
+    {
+        await accountRepository.CreateAccountAsync(new Shared.Account{Name = accName});
+    }
+
+    public async Task<Account> GetAccount(int accId)
+    {
+        return await accountRepository.GetAccountAsync(accId);
+    }
+
+    public async Task<List<Account>> GetAccounts()
+    {
+        return (await accountRepository.GetAccountsAsync()).ToList();
+    }
+}
