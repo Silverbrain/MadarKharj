b127aeb98b2959c3cae4e908cf901c0feb469e25
Sina Ataei <sinaataei@outlook.com>
2024-03-06 19:10:41 +0000

    The test console project replaced by xUnit test project

---
+++ PersonalFinance.Test/AccountRepository/AccountRepositoryTests.cs
@@ -0,0 +1,28 @@
+﻿using FluentAssertions;
+using Microsoft.EntityFrameworkCore.Migrations.Operations;
+using Microsoft.Extensions.Logging;
+using PersonalFinance.Repository;
+
+namespace PersonalFinance.Test;
+
+public class AccountRepositoryTests
+{
+    [Fact]
+    public async void AccountRepository_GetAccounts_ReturnList()
+    {
+        //Arrange
+        using ILoggerFactory factory = LoggerFactory.Create(builder =>
+        {
+            builder.AddConsole();
+        });
+        var logger = factory.CreateLogger(nameof(AccountRepository_GetAccounts_ReturnList));
+        var dbContext = new PFDbContext();
+        var accRepo = new AccountRepository(dbContext, logger);
+
+        //Act
+        var res = await accRepo.GetAccountsAsync();
+
+        //Assert
+        res.Should().NotBeNull();
+    }
+}
