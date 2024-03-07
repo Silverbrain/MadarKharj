ad7c9a61084522e8b2e9f1f675b8684d4dbdb4ae
Sina Ataei <sinaataei@outlook.com>
2024-03-07 10:04:34 +0000

    GetAccount/s test cases added

--- PersonalFinance.Repository/PFDbContext.cs
+++ PersonalFinance.Repository/PFDbContext.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Options;
 using PersonalFinance.Shared;
 
 namespace PersonalFinance.Repository;
@@ -6,16 +7,28 @@ namespace PersonalFinance.Repository;
 public class PFDbContext : DbContext
 {
     private readonly string _connectionString = "Server= localhost,1433; Database = SPFPDataBase; User Id = SA; Password = S!na8481780; MultipleActiveResultSets=true; TrustServerCertificate=True";
-
+    private readonly DbContextOptionsBuilder<PFDbContext> optionsBuilder = null;
+    
     public DbSet<Account> Accounts { get; set; }
     public DbSet<Entry> Entries { get; set; }
     public DbSet<Transaction> Transactions { get; set; }
 
+    public PFDbContext(DbContextOptions<PFDbContext> options) : base(options)
+    {
+    }
+
+    public PFDbContext()
+    {
+        optionsBuilder = new DbContextOptionsBuilder<PFDbContext>()
+            .UseSqlServer(_connectionString)
+            .EnableSensitiveDataLogging();
+    }
+
     protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
     {
-        base.OnConfiguring(optionsBuilder);
-        optionsBuilder.UseSqlServer(_connectionString);
-        optionsBuilder.EnableSensitiveDataLogging();
+        base.OnConfiguring(this.optionsBuilder ?? optionsBuilder);
+        // optionsBuilder.UseSqlServer(_connectionString);
+        // optionsBuilder.EnableSensitiveDataLogging();
     }
 
     protected override void OnModelCreating(ModelBuilder modelBuilder)
--- PersonalFinance.Test/AccountRepository/AccountRepositoryTests.cs
+++ PersonalFinance.Test/AccountRepository/AccountRepositoryTests.cs
@@ -1,5 +1,7 @@
-﻿using FakeItEasy;
+﻿using System.Runtime.CompilerServices;
+using FakeItEasy;
 using FluentAssertions;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Migrations.Operations;
 using Microsoft.Extensions.Logging;
 using PersonalFinance.Repository;
@@ -10,47 +12,103 @@ namespace PersonalFinance.Test;
 public class AccountRepositoryTests
 {
     private readonly ILogger logger;
-    private readonly PFDbContext dbContext;
-    private readonly IAccountRepository accRepo;
+
+    private async Task<PFDbContext> GetDbContext()
+    {
+        var dbContextOptions = new DbContextOptionsBuilder<PFDbContext>()
+            .UseInMemoryDatabase(databaseName: Guid.NewGuid().ToString())
+            .Options;
+
+        var databaseContext = new PFDbContext(dbContextOptions);
+
+        databaseContext.Database.EnsureCreated();
+
+        var accs = new List<Account>(){
+            new Account{ Id = 1, Name = "Income"},
+            new Account{ Id = 2, Name = "Expence"},
+            new Account{ Id = 3, Name = "Sina"}
+        };
+
+        if (await databaseContext.Accounts.CountAsync() <= 0)
+        {
+            await databaseContext.Accounts.AddRangeAsync(accs);
+            await databaseContext.SaveChangesAsync();
+        }
+
+        var trans = new List<Transaction>(){
+            new Transaction{ Id = Guid.NewGuid(), Date = DateTime.Now, Description = "Test1"},
+            new Transaction{ Id = Guid.NewGuid(), Date = DateTime.Now, Description = "Test2"},
+            new Transaction{ Id = Guid.NewGuid(), Date = DateTime.Now, Description = "Test3"}
+        };
+
+        if (await databaseContext.Transactions.CountAsync() <= 0)
+        {
+            await databaseContext.Transactions.AddRangeAsync(trans);
+            await databaseContext.SaveChangesAsync();
+        }
+
+        if (await databaseContext.Entries.CountAsync() <= 0)
+        {
+            await databaseContext.Entries.AddAsync(new Entry
+            { TransactionId = trans[0].Id, Amount = -1500, AccountId = 1, Id = Guid.NewGuid() });
+            await databaseContext.Entries.AddAsync(new Entry
+            { TransactionId = trans[0].Id, Amount = 1500, AccountId = 3, Id = Guid.NewGuid() });
+            await databaseContext.Entries.AddAsync(new Entry
+            { TransactionId = trans[1].Id, Amount = -500, AccountId = 3, Id = Guid.NewGuid() });
+            await databaseContext.Entries.AddAsync(new Entry
+            { TransactionId = trans[1].Id, Amount = 500, AccountId = 2, Id = Guid.NewGuid() });
+            await databaseContext.Entries.AddAsync(new Entry
+            { TransactionId = trans[2].Id, Amount = -200, AccountId = 3, Id = Guid.NewGuid() });
+            await databaseContext.Entries.AddAsync(new Entry
+            { TransactionId = trans[2].Id, Amount = 200, AccountId = 2, Id = Guid.NewGuid() });
+
+            await databaseContext.SaveChangesAsync();
+        }
+
+        return databaseContext;
+    }
     public AccountRepositoryTests()
     {
         //Dependencies
-        // logger = A.Fake<ILogger>();
-        // dbContext = A.Fake<PFDbContext>();
-        accRepo = A.Fake<IAccountRepository>(x =>
-        {
-            //x.WithArgumentsForConstructor(() => new AccountRepository(dbContext, logger));
-        });
+        logger = A.Fake<ILogger>();
     }
 
     [Fact]
     public async void AccountRepository_GetAccounts_ReturnList()
     {
         //Arrange
-        A.CallTo(() => accRepo.GetAccountsAsync())
-            .Returns(Task.FromResult(A.CollectionOfDummy<Account>(10).AsEnumerable()));
+        var dbContext = await GetDbContext();
+        var accRepo = new AccountRepository(dbContext, logger);
+
+        // A.CallTo(() => accRepo.GetAccountsAsync())
+        //     .Returns(Task.FromResult(A.CollectionOfDummy<Account>(10).AsEnumerable()));
 
         //Act
         var res = await accRepo.GetAccountsAsync();
 
         //Assert
         res.Should().BeOfType<List<Account>>();
-        res.Should().HaveCount(10);
+        res.Should().NotBeNullOrEmpty();
     }
 
     [Theory]
-    [InlineData(0)]
+    [InlineData(1)]
+    [InlineData(2)]
+    [InlineData(3)]
     public async void AccountRepository_GetAccount_ReturnAccount(int accountId)
     {
         //Arrange
-        //A.CallTo(() => accRepo.GetAccountAsync(accountId))
-            //.Returns(Task.FromResult(A.Dummy<Account>()));
+        var dbContext = await GetDbContext();
+        var accRepo = new AccountRepository(dbContext, logger);
+
+        // A.CallTo(() => accRepo.GetAccountAsync(accountId))
+        //     .Returns(Task.FromResult(A.Dummy<Account>()));
 
         //Act
         var res = await accRepo.GetAccountAsync(accountId);
 
         //Assert
-        res.Should().BeOfType<Account?>();
         res.Should().NotBeNull();
+        res.Should().BeOfType<Account?>();
     }
 }
