b324030267a46dadd0fd5a6c5f2246898f83ee00
Sina Ataei <sinaataei@outlook.com>
2024-03-07 10:16:48 +0000

    Database configuration process changed

--- PersonalFinance.Repository/PFDbContext.cs
+++ PersonalFinance.Repository/PFDbContext.cs
@@ -7,8 +7,7 @@ namespace PersonalFinance.Repository;
 public class PFDbContext : DbContext
 {
     private readonly string _connectionString = "Server= localhost,1433; Database = SPFPDataBase; User Id = SA; Password = S!na8481780; MultipleActiveResultSets=true; TrustServerCertificate=True";
-    private readonly DbContextOptionsBuilder<PFDbContext> optionsBuilder = null;
-    
+
     public DbSet<Account> Accounts { get; set; }
     public DbSet<Entry> Entries { get; set; }
     public DbSet<Transaction> Transactions { get; set; }
@@ -19,18 +18,21 @@ public class PFDbContext : DbContext
 
     public PFDbContext()
     {
-        optionsBuilder = new DbContextOptionsBuilder<PFDbContext>()
-            .UseSqlServer(_connectionString)
-            .EnableSensitiveDataLogging();
     }
 
     protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
     {
-        base.OnConfiguring(this.optionsBuilder ?? optionsBuilder);
-        // optionsBuilder.UseSqlServer(_connectionString);
-        // optionsBuilder.EnableSensitiveDataLogging();
+        if (optionsBuilder.IsConfigured)
+        {
+            base.OnConfiguring(optionsBuilder);
+        }
+        else
+        {
+            optionsBuilder.UseSqlServer(_connectionString);
+            optionsBuilder.EnableSensitiveDataLogging();
+            base.OnConfiguring(optionsBuilder);
+        }
     }
-
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
         base.OnModelCreating(modelBuilder);
