af46454e70aba5ab7dcea9eb72199c97a978095a
Sina Ataei <sinaataei@outlook.com>
2024-03-11 19:10:47 +0000

    Services implemented

--- PersonalFinance.Service/TransactionService.cs
+++ PersonalFinance.Service/TransactionService.cs
@@ -1,6 +1,52 @@
-﻿namespace PersonalFinance.Service;
+﻿using PersonalFinance.Shared;
+using PersonalFinance.Repository;
 
-public class TransactionService
+namespace PersonalFinance.Service;
+
+public class TransactionService : ITransactionService
 {
+    private readonly ITransactionRepository transactionRepository;
 
+    public TransactionService(ITransactionRepository transactionRepository)
+    {
+        this.transactionRepository = transactionRepository;
+    }
+    public async Task<Transaction> CreateTransactionAsync(Transaction transaction)
+    {
+        if (transaction == null)
+        {
+            throw new Exception("Transaction object pased to this function is null", new NullReferenceException());
+        }
+        return await transactionRepository.CreateTransactionAsync(transaction);
+    }
+
+    public async Task<Transaction> DeleteTransactionAsync(Guid transactionId)
+    {
+        return await transactionRepository.DeleteTransactionAsync(transactionId);
+    }
+
+    public async Task<Transaction> GetTransactionAsync(Guid transactionId)
+    {
+        return await transactionRepository.GetTransactionAsync(transactionId);
+    }
+
+    public async Task<IEnumerable<Transaction>> GetTransactionsAsync()
+    {
+        return await transactionRepository.GetTransactionsAsync();
+    }
+
+    public async Task<IEnumerable<Transaction>> SearchTransactionsAsync(string description)
+    {
+        return await transactionRepository.SearchTransactionAsync(description);
+    }
+
+    public async Task<Transaction> UpdateTransactionAsync(Transaction transaction)
+    {
+        if (transaction == null)
+        {
+            throw new Exception("Transaction object pased to this function is null", new NullReferenceException());
+        }
+
+        return await transactionRepository.UpdateTransactionAsync(transaction);
+    }
 }
