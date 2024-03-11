ec9330744e17665c3f673df98e3c95bb3f639cb5
Sina Ataei <sinaataei@outlook.com>
2024-03-11 18:54:30 +0000

    AccountService Implemented

--- PersonalFinance.Service/AccountService.cs
+++ PersonalFinance.Service/AccountService.cs
@@ -1,6 +1,52 @@
-﻿namespace PersonalFinance.Service;
+﻿using PersonalFinance.Repository;
+using PersonalFinance.Shared;
 
-public class AccountService
+namespace PersonalFinance.Service;
+
+public class AccountService : IAccountService
 {
+    private readonly IAccountRepository accountRepository;
 
+    public AccountService(IAccountRepository accountRepository)
+    {
+        this.accountRepository = accountRepository;
+    }
+    public async Task<Account> CreateAccountAsync(Account account)
+    {
+        if (account != null)
+        {
+            var res = await accountRepository.CreateAccountAsync(account);
+            return res;
+        }
+        throw new Exception("Account object pased to this function is null", new NullReferenceException());
+    }
+
+    public async Task<Account> DeleteAccountAsync(int accountId)
+    {
+        return await accountRepository.DeleteAccountAsync(accountId);
+    }
+
+    public async Task<Account> GetAccountAsync(int accountId)
+    {
+        return await accountRepository.GetAccountAsync(accountId);
+    }
+
+    public async Task<IEnumerable<Account>> GetAccountsAsync()
+    {
+        return await GetAccountsAsync();
+    }
+
+    public Task<IEnumerable<Account>> SearchAccountAsync(string accountName)
+    {
+        return accountRepository.SearchAccountAsync(accountName);
+    }
+
+    public async Task<Account> UpdateAccountAsync(Account account)
+    {
+        if (account == null)
+        {
+            throw new Exception("Account object pased to this function is null", new NullReferenceException());
+        }
+        return await accountRepository.UpdateAccountAsync(account);
+    }
 }
