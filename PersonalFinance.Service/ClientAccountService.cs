7509f39815d9f2c439a6ddbeeea1f42cd730345a
Sina Ataei <sinaataei@outlook.com>
2024-03-12 11:45:28 +0000

    Account API controller completed

---
+++ PersonalFinance.Service/ClientAccountService.cs
@@ -0,0 +1,46 @@
+﻿using System.Net.Http;
+using System.Net.Http.Json;
+using Microsoft.Identity.Client;
+using PersonalFinance.Shared;
+
+namespace PersonalFinance.Service;
+
+public class ClientAccountService : IAccountService
+{
+    private readonly HttpClient httpClient;
+
+    public ClientAccountService(HttpClient httpClient)
+    {
+        this.httpClient = httpClient;
+    }
+
+    public Task<Account> CreateAccountAsync(Account account)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<Account> DeleteAccountAsync(int accountId)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<Account> GetAccountAsync(int accountId)
+    {
+        throw new NotImplementedException();
+    }
+
+    public async Task<IEnumerable<Account>> GetAccountsAsync()
+    {
+        return await httpClient.GetFromJsonAsync<IEnumerable<Account>>("/api/account");
+    }
+
+    public Task<IEnumerable<Account>> SearchAccountAsync(string accountName)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<Account> UpdateAccountAsync(Account account)
+    {
+        throw new NotImplementedException();
+    }
+}
---
+++ PersonalFinance/Controllers/AccountController.cs
@@ -0,0 +1,88 @@
+﻿using System.Net.Http.Json;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.ModelBinding;
+using Microsoft.EntityFrameworkCore.Metadata.Internal;
+using PersonalFinance.Components.Pages;
+using PersonalFinance.Service;
+using PersonalFinance.Shared;
+
+namespace PersonalFinance;
+
+[Route("/api/[controller]")]
+[ApiController]
+public class AccountController : ControllerBase
+{
+    private readonly IAccountService accountService;
+
+    public AccountController(IAccountService accountService)
+    {
+        this.accountService = accountService;
+    }
+
+    [HttpGet]
+    public async Task<ActionResult<IEnumerable<Account>>> GetAccounts()
+    {
+        return Ok(await accountService.GetAccountsAsync());
+    }
+
+    [HttpGet("{id:int}")]
+    public async Task<ActionResult<Account>> GetAccount(int id)
+    {
+        var acc = await accountService.GetAccountAsync(id);
+        if (acc == null)
+        {
+            return NotFound();
+        }
+
+        return Ok(acc);
+    }
+
+    [HttpPost]
+    public async Task<ActionResult<Account>> CreateAccount(Account account)
+    {
+        if (account == null)
+        {
+            return BadRequest("Account object can not be null.");
+        }
+
+        var res = await accountService.CreateAccountAsync(account);
+
+        return CreatedAtAction(nameof(GetAccount), new { id = res.Id }, res);
+    }
+
+    [HttpPut("{id:int}")]
+    public async Task<ActionResult> UpdateAccount(int id, Account account)
+    {
+        if (id != account.Id)
+        {
+            return BadRequest();
+        }
+
+        var updatedAcc = await accountService.UpdateAccountAsync(account);
+
+        if (updatedAcc == null)
+        {
+            return NotFound();
+        }
+
+        return NoContent();
+    }
+
+    [HttpDelete("{id:int}")]
+    public async Task<ActionResult> DeleteAccount(int id)
+    {
+        if (id == null)
+        {
+            return BadRequest();
+        }
+
+        var res = await accountService.DeleteAccountAsync(id);
+
+        if(res == null)
+        {
+            return NotFound();
+        }
+
+        return NoContent();
+    }
+}
