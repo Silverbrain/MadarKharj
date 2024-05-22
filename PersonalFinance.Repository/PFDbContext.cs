5b6757f0f8765d5ea3569db4fe7dd4504db654f6
Sina Ataei <sinaataei@outlook.com>
2024-05-22 16:35:09 +0100

    DBContext class added

---
+++ PersonalFinance.Repository/PFDbContext.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Options;
+using PersonalFinance.Shared;
+
+namespace PersonalFinance.Repository;
+
+public class PFDbContext : IdentityDbContext<ApplicationUser>
+{
+    private readonly string _connectionString = "";
+
+    public DbSet<Account> Accounts { get; set; }
+    public DbSet<Entry> Entries { get; set; }
+    public DbSet<Transaction> Transactions { get; set; }
+
+    public PFDbContext(DbContextOptions<PFDbContext> options) : base(options)
+    {
+    }
+    
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        if (optionsBuilder.IsConfigured)
+        {
+            base.OnConfiguring(optionsBuilder);
+        }
+        else
+        {
+            optionsBuilder.UseSqlServer(_connectionString);
+            optionsBuilder.EnableSensitiveDataLogging();
+            base.OnConfiguring(optionsBuilder);
+        }
+    }
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        base.OnModelCreating(modelBuilder);
+        //modelBuilder.SeedWithData(new DataSeed());
+    }
+}
